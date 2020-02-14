0996db7087ce102186bba336255e164d9e04cf0d
Laura Lujan <lauralujanhdez@gmail.com>
2020-02-14 16:37:01 -0600

    first commit lauralujan

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var links = document.getElementsByTagName("a")
+var numberOfLinks = links.length
+
+
+function createLists(x){
+    let newli = document.createElement("li");
+    let  newlicontent = document.createTextNode(x);
+    newli.appendChild(newlicontent);
+    document.body.appendChild(newli);
+}
+
+createLists(numberOfLinks);
+
+var lastLink = links[numberOfLinks-1]
+
+createLists(lastLink)
+
+var penultimo = links[numberOfLinks-2]
+createLists(penultimo);
+
+var wemanLinks = document.querySelectorAll
+
+var parrafo = document.getElementsByTagName("p");
+
+var lastParr = parrafo[parrafo.length -1]
+
+var linksInParr = lastParr.getElementsByTagName("a");
+var numofLinksInParr = linksInParr.length
+
+createLists(numofLinksInParr);
+
+//////////////
+
+
+function showOnClic(){
+    var element = document.getElementsByTagName("span");
+    element[0].classList.remove("hidden");
+}
+
+
+//////
+function addOnClic(){
+   let newli = document.createElement("li");
+   let  newlicontent = document.createTextNode("x");
+   newli.appendChild(newlicontent);
+   document.body.appendChild(newli);
+}
